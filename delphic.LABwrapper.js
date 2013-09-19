58992f65958c8863b4620deeda347e769da83fe6
delphic-digital <jmills@delphicdigital.com>
2013-09-19 15:36:04 -0400

    Create delphic.LABwrapper.js

---
+++ delphic.LABwrapper.js
@@ -0,0 +1,21 @@
+$LAB.setGlobalDefaults({AlwaysPreserveOrder:true});
+
+function loadjs(f,c){
+	var queue = [];
+	
+	if (f instanceof Array) {
+		for (var i=0, len=f.length; i<len; i++) {
+			queue.push(f[i]);
+		}
+	}else if (typeof f == "string") { 
+		queue = LABconfig.shortcuts[f];
+	}
+	for (var i=0, len=queue.length; i<len; i++) {
+		if (LABconfig.paths[queue[i]]){
+			$LAB = $LAB.script(LABconfig.paths[queue[i]]);
+		}else{
+			$LAB.script(queue[i]);
+		}	
+	}
+	$LAB = $LAB.wait(c);
+}
