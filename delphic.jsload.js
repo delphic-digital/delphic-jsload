c7a33d8ddcb026888a3a51e5047619c2d517928c
delphic-digital <jmills@delphicdigital.com>
2014-04-14 17:19:33 -0400

    Fix namespace

--- delphic.jsload.js
+++ delphic.jsload.js
@@ -2,7 +2,7 @@
 
 $LAB.setGlobalDefaults({AlwaysPreserveOrder:true});
 
-DELPHICSITE.jsload = function(f,c){
+DELPHIC.jsload = function(f,c){
 	var queue = [];
 	
 	if (f instanceof Array) {
@@ -10,11 +10,11 @@ DELPHICSITE.jsload = function(f,c){
 			queue.push(f[i]);
 		}
 	}else if (typeof f == "string") { 
-		queue = DELPHICSITE.LABconfig.shortcuts[f];
+		queue = DELPHIC.LABconfig.shortcuts[f];
 	}
 	for (var i=0, len=queue.length; i<len; i++) {
-		if (DELPHICSITE.LABconfig.paths[queue[i]]){
-			$LAB = $LAB.script(DELPHICSITE.LABconfig.paths[queue[i]]);
+		if (DELPHIC.LABconfig.paths[queue[i]]){
+			$LAB = $LAB.script(DELPHIC.LABconfig.paths[queue[i]]);
 		}else{
 			$LAB.script(queue[i]);
 		}	
